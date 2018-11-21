b524d853f60411a0c7e1710d2e7638998db2849a
Vlad Denisov <vlad@vdenisov.net>
2018-11-21 18:11:54 -0500

    web server example

--- server.js
+++ server.js
@@ -1,25 +1,50 @@
 const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
 
 let app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
 
 /**
  * Middleware helps you add additional configurations to process requests.
  */
+app.use((req, res, next) => {
+    let now = new Date().toString();
+    let log = `${now}: ${req.method} ${req.originalUrl}`;
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if(err) {
+            console.log('Unable to append to server.log');
+        }
+    });
+    next();
+});
+app.use((req, res, next) => {
+    res.render('maintenance.hbs');
+});
 app.use(express.static(`${__dirname}/public`));
 
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+
 app.get('/', (req, res) => {
-    // res.send('<h1>Hello Express</h1>\n');
-    res.send({
-        name: 'Vlad',
-        likes: [
-            'Running',
-            'Walking'
-        ]
+    res.render('home.hbs',{
+        pageTitle: 'Home Page',
+        welcomeMessage: 'Hello World!'
     });
 });
 
 app.get('/about', (req, res) => {
-    res.send('<h1>About Page</h1>');
+    res.render('about.hbs', {
+        pageTitle: 'About Page'
+    });
 });
 
 app.get('/bad', (req, res) => {
